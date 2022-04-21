93a469d24d849eae3575e4e2bcd6a32c3c9aeed6
SLINIcraftet204 <slinicraftet204@chaos-craft.de>
2022-04-21 15:36:31 +0200

    fixed app crash on Server API call while offline usage also changed minor things like app themes and SDK-compile version

--- app/src/main/java/de/slini_haeher/pointsalike/MainActivity.kt
+++ app/src/main/java/de/slini_haeher/pointsalike/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.net.URL
+import java.net.UnknownHostException
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,13 +25,16 @@ private class DownloadFilesTask(private var m: MainActivity) : AsyncTask<String?
             //val text: TextView = m.findViewById(R.id.textcount)
             //tv1.text = "Hello my New Project"
             //val text = m.findViewById(0) as TextView
-            session_id = URL("http://mc.chaos-craft.de:24000/pointsalike/login?user=noroot&pw=o7nq4L2zaK3QLYjryscFB5RvxR2HG7zN4sLwKSMjEMy4CXU3Smd4pT4BXpeCm7V2").readText()
-            val t = "ID: " + session_id
-            //text.setText(t)
+            if(session_id == "" || session_id == "Offline" || session_id == "not found")
+                session_id = URL("http://mc.chaos-craft.de:24000/pointsalike/login?user=noroot&pw=o7nq4L2zaK3QLYjryscFB5RvxR2HG7zN4sLwKSMjEMy4CXU3Smd4pT4BXpeCm7V2").readText()
             url = URL("http://mc.chaos-craft.de:24000/pointsalike/getCount?user=noroot&sessionId="+session_id+"").readText()
         } catch (e: java.io.FileNotFoundException){
             e.printStackTrace()
             url = "not found"
+            session_id = "not available"
+        } catch (e: UnknownHostException){
+            url = "You are Offline"
+            session_id = "Offline"
         }
         return 10L
     }
@@ -40,7 +44,7 @@ private class DownloadFilesTask(private var m: MainActivity) : AsyncTask<String?
 
     override fun onPostExecute(result: Long) {
         val text = m.findViewById(R.id.textcount) as TextView
-        val t = "Points: " + url.substring(2, url.length-3)
+        val t = "Points: " + url
         text.setText(t)
     }
 }
